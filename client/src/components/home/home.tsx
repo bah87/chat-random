50fddff2e29333eb5f8323e33aba7489de0d2f12
Brendan Higgins <bhiggins033@gmail.com>
2018-12-15 15:36:25 -0500

    Initial server and socket scaffolding

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -1,19 +1,46 @@
 import * as React from "react";
+import io from "socket.io-client";
 
 export interface IHomeProps {
   readonly username: string;
 }
 
+export interface IHomeState {
+  readonly message: string;
+  readonly socket: SocketIOClient.Socket;
+}
+
 export class Home extends React.Component<IHomeProps> {
+  public state: IHomeState;
+
   constructor(props: IHomeProps) {
     super(props);
+    this.state = { message: "", socket: io("http://localhost:3001") };
   }
+
+  handleChange = (e: any) => {
+    this.setState({ message: e.currentTarget.value });
+  };
+
+  handleSend = () => {
+    console.log("Sending message: ", this.state.message);
+    this.state.socket.emit("chat message", this.state.message);
+  };
 
   render() {
     return (
-      <div>{`Welcome ${
-        this.props.username
-      }! Waiting on the next available user`}</div>
+      <div>
+        <div>{`Welcome ${
+          this.props.username
+        }! Waiting on the next available user`}</div>
+        <input
+          type="text"
+          placeholder="Message..."
+          onChange={this.handleChange}
+          value={this.state.message}
+        />
+        <button onClick={this.handleSend}>Send</button>
+      </div>
     );
   }
 }
---
+++ server/app.js
@@ -0,0 +1,24 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.get("/", function(req, res) {
+  res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+
+  socket.on("chat message", function(message) {
+    console.log("chat message sent: ", message);
+    io.emit("chat message", message);
+  });
+});
+
+io.on("disconnect", function() {
+  console.log("user disconnected");
+});
+
+http.listen(3001, function() {
+  console.log("listening on *:3001");
+});
