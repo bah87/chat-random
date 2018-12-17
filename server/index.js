22b9a63b5b0030212da7b24e3560dac572a9377a
Brendan Higgins <bhiggins033@gmail.com>
2018-12-16 23:24:29 -0500

    Remove unused code from server index

--- server/index.js
+++ server/index.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const app = express();
-// var http = require("http").Server(app);
-// var io = require("socket.io")(http);
-// var uuid = require("uuid");
-// var moment = require("moment");
 const mongoose = require("mongoose");
 const config = require("./config");
 const socketEvents = require("./socket/socket-events");
@@ -18,20 +14,3 @@ const server = app.listen(config.port, function() {
 
 const io = require("socket.io").listen(server);
 socketEvents(io);
-
-// io.on("connection", function(socket) {
-//   console.log("a user connected", socket.id);
-
-//   socket.on("chat message", function(message) {
-//     console.log("chat message sent: ", message);
-//     io.emit("chat message", {
-//       ...message,
-//       id: uuid(),
-//       time: moment().format()
-//     });
-//   });
-
-//   socket.on("disconnect", function() {
-//     console.log("user disconnected", socket.id);
-//   });
-// });
